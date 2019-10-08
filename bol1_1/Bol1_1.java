c78e03e684184d34c5b46be7adb9a0030649cef4
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-08 08:42:26 +0200

    inicio

---
+++ bol1_1/Bol1_1.java
@@ -0,0 +1,21 @@
+
+package bol1_1;
+
+
+
+public class Bol1_1 {
+
+  
+    public static void main(String[] args) {
+        //exercicio 1
+        int b = 4,h = 3, A;
+        A = (b*h)/2;
+        System.out.println("El area de un triangulo de base=4 y altura=3 es igual a "+A);
+       
+        
+        
+        
+        
+    }
+    
+}
